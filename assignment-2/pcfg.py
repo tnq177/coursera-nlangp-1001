4b30a3dda9fd4bea7a092589f7729141ad8f917b
Toan Nguyen <toannq177@gmail.com>
2015-12-15 09:47:40 +0700

    [f] Assignment 2 - Part 2
    Still have no idea why my result doesn’t match https://github.com/mprinc/NLP/tree/master/nlangp-001-coursera

--- assignment-2/pcfg.py
+++ assignment-2/pcfg.py
@@ -1,7 +1,8 @@
 #! /usr/bin/python
 from __future__ import print_function, division
 import sys
-
+from decimal import *
+import json
 
 class PCFG(object):
 
@@ -10,7 +11,6 @@ class PCFG(object):
         self.binary_count = {}
         self.unary_count = {}
         self.word_count = {}
-        self.words = []
 
         with open(count_file, 'r') as count_file_handle:
             for line in count_file_handle:
@@ -36,10 +36,10 @@ class PCFG(object):
     def replace_rare(self):
         unary_count_keys = self.unary_count.keys()
         nonterminals_or_words = list(set([x[1] for x in unary_count_keys]))
-        self.words = [
+        words = [
             x for x in nonterminals_or_words if x not in self.nonterminals]
 
-        for word in self.words:
+        for word in words:
             word_count = sum(
                 [self.unary_count.get((nonterminal, word), 0) for nonterminal in self.nonterminals])
 
@@ -51,14 +51,77 @@ class PCFG(object):
 
                 for nonterminal in self.nonterminals:
                     self.unary_count.setdefault((nonterminal, '_RARE_'), 0)
-                    self.unary_count[(nonterminal, '_RARE_')] += word_count
+                    self.unary_count[(nonterminal, '_RARE_')] += self.unary_count.get((nonterminal, word), 0)
+
+    def binary_prob(self, key):
+        return self.binary_count.get(key, 0) / self.nonterminal_count[key[0]]
+
+    def unary_prob(self, key):
+        _key = list(key)
+        _key[1] = _key[1] if _key[1] in self.nonterminals else self.replace_word(_key[1])
+        _key = tuple(_key)
+        return self.unary_count.get(_key, 0) / self.nonterminal_count[_key[0]]
+
+    def replace_word(self, word):
+        if word not in self.word_count or self.word_count[word] < 5:
+            return '_RARE_'
+        else:
+            return word
+
+    def parse(self, sent):
+        '''
+        Expect sentence to be a string
+        '''
+        sent = sent.replace('\n', '').split(' ')
+        n = len(sent)
+        sent = [''] + sent
+
+        pi, bp = {}, {}
+        for i in xrange(1, n + 1):
+            xi = sent[i]
+            for X in self.nonterminals:
+                pi[i, i, X] = Decimal(self.unary_prob((X, xi)))
+                bp[i, i, X] = xi 
+
+        for l in xrange(1, n):
+            for i in xrange(1, n - l + 1):
+                j = i + l 
+
+                max_prob = {}
+                max_nonterminals = {}
+                for s in xrange(i, j):
+                    for key, count in self.binary_count.iteritems():
+                        X, Y, Z = key
+                        max_prob.setdefault(X, -1)
+                        max_nonterminals.setdefault(X, '')
+
+                        prob = Decimal(self.binary_prob(key)) * pi.get((i, s, Y), Decimal(0)) * pi.get((s + 1, j, Z), Decimal(0))
+
+                        if prob > max_prob[X]:
+                            max_prob[X] = prob
+                            max_nonterminals[X] = (Y, Z, s)
+
+                for key, count in self.binary_count.iteritems():
+                    X = key[0]
+                    pi[i, j, X] = max_prob[X]
+                    bp[i, j, X] = max_nonterminals[X]
+
+        return bp, n
+
+    def tree_to_array(self, bp, start_nonterminal, n):
+        def _to_array(start, end, key):
+            if start == end:
+                return [key, bp[start, end, key]]
+            else:
+                Y, Z, s = bp[start, end, key]
+                return [key, _to_array(start, s, Y), _to_array(s + 1, end, Z)]
+
+        return _to_array(1, n, start_nonterminal)
 
 
-def write_new_count_file():
+def write_new_count_file(pcfg):
     count_file = './cfg.counts'
     out_file = './parse_train.counts.out'
-
-    pcfg = PCFG(count_file)
 
     # Check if unary line with rare word has already been written down
     visited = {}
@@ -93,15 +156,34 @@ def write_new_count_file():
     out_file_handle.close()
     count_file_handle.close()
 
+def main(args):
+    if len(args) < 2:
+        sys.exit(1)
 
-def main(partId):
-    if partId == '1':
-        write_new_count_file()
+    pcfg = PCFG('./cfg.counts')
+
+    if args[1] == '1':
+        write_new_count_file(pcfg)
+    elif args[1] == '2':
+        if len(args) < 4:
+            sys.exit(1)
+
+        open(args[3], 'w').close()
+        test_file_handle = open(args[2], 'r')
+        out_file_handle = open(args[3], 'w')
+
+        for line in test_file_handle:
+            sent = line.replace('\n', '')
+            if sent:
+                bp, n = pcfg.parse(sent)
+                array_json_string = json.dumps(pcfg.tree_to_array(bp, 'SBARQ', n))
+                out_file_handle.write('{0}\n'.format(array_json_string))
+
+        test_file_handle.close()
+        out_file_handle.close()
+
     else:
         print('Not implemented!')
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        sys.exit(1)
-    else:
-        main(sys.argv[1])
+    main(sys.argv)
